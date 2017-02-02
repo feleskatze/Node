595a93bc66de31d6df04497a1ab2799dc4e58141
feleskatze <no0risk0no0return.03@gmail.com>
2017-02-02 15:36:05 +0900

    ejsとか

--- test-server.js
+++ test-server.js
@@ -1,9 +1,19 @@
-var http = require('http');
+var settings = require('./settings');
+var http = require('http'),
+    fs =require("fs");
 var server = http.createServer();
 server.on('request', function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Hello World');
-	res.end();
+ fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
+  if(err){
+   res.writeHead(404, {'Content-Type': 'text/plain'});
+   res.write('not found');
+   return res.end();
+  }
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();
+
+ })
 });
-server.listen(3000,'192.168.11.16');
+server.listen(settings.port,settings.host);
 console.log("server listening ...");
